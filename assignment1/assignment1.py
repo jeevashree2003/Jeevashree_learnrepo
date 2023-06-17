2e05c07be14996ede8cfaed90ca44f73ec00ae78
Jeevashree S <125792671+jeevashree2003@users.noreply.github.com>
2023-06-17 07:30:10 +0530

    Add files via upload

---
+++ assignment1/assignment1.py
@@ -0,0 +1,5 @@
+print("Hello everyone!")
+a=int(input("enter a number1"))
+b=int(input("enter another number2"))
+c=a+b
+print("Number 1 + Number 2=",c)
---
+++ assignment3/app.py
@@ -0,0 +1,40 @@
+"""
+Question:
+Deploy the previous two assignments on a flask api and we should be able to run this by calling.
+/api/printHello
+/api/modifyRecepie
+"""
+
+# Importing the required libraries
+from flask_app import Flask,jsonify
+
+# Importing the required functions from the second assignment file
+from data.assignment_2 import read_data,mod,hello
+
+# Defining the flask app
+app=Flask(__name__)
+
+# Defining the routes
+@app.route('/api/printHello',methods=['GET']) 
+# Defining the route for the hello world 
+def printHello():
+    data=hello()
+    return jsonify(data)
+
+@app.route('/api/originalRecepie',methods=['GET'])
+# Defining the route for the original recepie
+def printData():
+    data=read_data()
+    return jsonify(data)
+
+@app.route('/api/modifiedRecepie',methods=['GET']) 
+# Defining the route for the modified recepie
+def printModData():
+    data=mod()
+    return jsonify(data)
+
+# Defining the main function
+if __name__=='__main__':
+    app.run(debug=True,port=80)
+    
+    
